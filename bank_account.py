8292f9c4fefa68bb6af6c4f888018c60e67209f8
Jessie Chen <yhcjessie1@gmail.com>
2020-12-02 14:58:38 -0500

    bank_account with Inheritance refactor

--- bank_account.py
+++ bank_account.py
@@ -1,25 +1,23 @@
 class BankAccount:
     def __init__(self):
         self.balance = 0
-        self.overdraft_fees=0
+        self.interest = 1.02
     def deposit(self, amount):
         self.balance += amount
         return self.balance
     
     def withdraw(self, amount):
-        net_balance=self.balance-amount-self.overdraft_fees
+        net_balance=self.balance-amount
         self.balance-=amount if net_balance>=-100 else 0
         return self.balance
-    def accumulate_interest(self, interest):
-        self.balance= self.balance +self.balance*interest
-        accu_int_amount=self.balance*interest
-        return (self.balance,accu_int_amount)
+    def accumulate_interest(self):
+        self.balance= self.balance*self.interest
+        return self.balance
 class ChildrensAccount(BankAccount):
     def __init__(self):
         super().__init__()
-    def accumulate_interest(self, amount):
-        amount=10
-        self.balance+= amount
+    def accumulate_interest(self):
+        self.balance+= 10
         # return self.balance
         print(f"Child's account has ${childs_account.balance}")
 class OverdraftAccount(BankAccount):
@@ -34,11 +32,11 @@ class OverdraftAccount(BankAccount):
 basic_account = BankAccount()
 basic_account.deposit(600)
 basic_account.withdraw(17)
-basic_account.accumulate_interest(0.02)
+basic_account.accumulate_interest()
 childs_account = ChildrensAccount()
 childs_account.deposit(34)
 childs_account.withdraw(17)
-childs_account.accumulate_interest(0.02)
+childs_account.accumulate_interest()
 overdraft_account = OverdraftAccount()
 overdraft_account.deposit(12)
 overdraft_account.withdraw(17)
