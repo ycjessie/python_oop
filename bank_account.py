746c614022938c9a636f8de03639a8a90454db30
Jessie Chen <yhcjessie1@gmail.com>
2020-12-02 15:23:17 -0500

    bank_account with refactoring

--- bank_account.py
+++ bank_account.py
@@ -1,34 +1,52 @@
-#https://git.generalassemb.ly/prudential-0921/python-oop/blob/master/readme.md
+# https://git.generalassemb.ly/prudential-0921/python-oop/blob/master/readme.md
 class BankAccount:
     def __init__(self):
         self.balance = 0
         self.interest = 1.02
+
     def deposit(self, amount):
         self.balance += amount
         return self.balance
-    
+
     def withdraw(self, amount):
-        net_balance=self.balance-amount
-        self.balance-=amount if net_balance>=-100 else 0
+        net_balance = self.balance-amount
+        self.balance -= amount if net_balance >= -100 else 0
         return self.balance
+
     def accumulate_interest(self):
-        self.balance= self.balance*self.interest
+        self.balance *= self.interest
         return self.balance
+
+#ChildrensAccount class inherits from BankAccount
 class ChildrensAccount(BankAccount):
     def __init__(self):
         super().__init__()
+        #ChildrensAccount has no interest rate
+        self.interest_rate=0.0
+    # accumulate_interest method
     def accumulate_interest(self):
-        self.balance+= 10
-        # return self.balance
-        print(f"Child's account has ${childs_account.balance}")
+        self.balance += 10
+        return self.balance
+        
+
+
 class OverdraftAccount(BankAccount):
     def __init__(self):
-        super().__init__()  
+        super().__init__()
+        self.interest_rate=0.0
+        #an overdraft_penalty property that defaults to $40.
+        self.overdraft_penalty = 40
+    
     def withdraw(self, amount):
-        overdraft_penalty=40
-        self.balance-=amount if self.balance<0 else overdraft_penalty
-
-        return self.balance     
+        if amount>self.balance:
+            self.balance -= self.overdraft_penalty
+            return False
+        else: return super().withdraw(amount)
+    def accumulate_interest(self):
+        if self.balance<=0:
+                return self.balance 
+        #force to reference Parent class
+        else:return super().accumulate_interest()
 
 basic_account = BankAccount()
 basic_account.deposit(600)
@@ -37,7 +55,11 @@ basic_account.accumulate_interest()
 childs_account = ChildrensAccount()
 childs_account.deposit(34)
 childs_account.withdraw(17)
+print(f"Child's account has ${childs_account.balance}")
 childs_account.accumulate_interest()
 overdraft_account = OverdraftAccount()
 overdraft_account.deposit(12)
 overdraft_account.withdraw(17)
+print(f"Overdraft account has ${overdraft_account.balance}")
+overdraft_account.accumulate_interest()
+print(f"Overdraft account has ${overdraft_account.balance}")
